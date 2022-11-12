58936c3ddadc5f8c51e43727983ad228175cd781
Rajesh Pal <rajeshpal1984@users.noreply.github.com>
2022-11-12 22:07:06 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,17 @@ const http = require('http');
 // Setting Port Number as 80
 const port = 80;
 
+/*
+#!/bin/bash
+yum install httpd -y
+service httpd start
+chkconfig httpd on
+
+wget https://github.com/rajeshpal1984/aws_node/archive/refs/heads/main.zip
+unzip main.zip
+cp -r aws_node-main/* -d /var/www/html/
+*/
+
 // Setting hostname as the localhost
 // NOTE: You can set hostname to something
 // else as well, for example, say 127.0.0.1
